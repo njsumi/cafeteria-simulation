c44ec0823ea969bb0ae42117d9b92d7ab13ce5bb
Nina Gruteser <94321646+njsumi@users.noreply.github.com>
2022-01-30 16:18:08 -0500

    Add files via upload

---
+++ campus center project/burger.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class burger here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class burger extends food
+{
+    /**
+     * Act - do whatever the burger wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
